67b1b53d562f5cd27064abca34ea8410fb6f2a68
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-03-08 17:12:51 +0500

    создан класс виртуального окружения

---
+++ code/interpreter.py
@@ -0,0 +1,14 @@
+class Environment:
+    def __init__(self):
+        self.variables = {}
+
+    def define(self, name, value):
+        self.variables[name] = value
+
+    def get(self, name):
+        if name in self.variables:
+            return self.variables[name]
+        raise Exception(f'Undefined variable: {name}')
+
+
+

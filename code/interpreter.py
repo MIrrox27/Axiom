8aa3c1829ae97eefc8de19b4e6463eb75ef5e9e7
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-03-08 17:15:27 +0500

    создан класс интерпретатора

--- code/interpreter.py
+++ code/interpreter.py
@@ -12,3 +12,30 @@ class Environment:
 
 
 
+class Interpreter:
+    def __init__(self):
+        self.env = Environment() # класс с глобальным окружением
+
+    def visit(self, node):
+        method_name = f'visit_{type(node).__name__}'
+        method = getattr(self, method_name, self.visit_error)
+        return method(node)
+
+    def visit_error(self, node):
+        raise Exception(f'No visit method for {type(node).__name__}')
+
+    def visit_Literal(self, node):
+        return node.value
+
+    def visit_Identifier(self, node):
+        return self.env.get(node.name)
+
+
+
+
+
+
+
+
+
+

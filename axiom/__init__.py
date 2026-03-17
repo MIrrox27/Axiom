0e8165eb758b435f27b8d23183a50b136e65a035
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-03-17 14:14:37 +0500

    написан __init__

---
+++ axiom/__init__.py
@@ -0,0 +1,12 @@
+# author https://github.com/MIrrox27/Axiom
+# __init__.py
+
+
+__version__ = "0.1.0"
+
+from axiom.AxiomTokens import *
+from axiom.AxiomLexer import *
+from axiom.AxiomParser import *
+from axiom.AxiomASTNodes import *
+from axiom.AxiomInterpreter import *
+from axiom.repl import *

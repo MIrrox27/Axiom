6acc64906c54db7bf1ab426973287496811bfe08
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-03-17 14:14:51 +0500

    написан __main__.py

---
+++ axiom/__main__.py
@@ -0,0 +1,78 @@
+# author https://github.com/MIrrox27/Axiom
+# __main__.py
+
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from axiom.AxiomTokens import *
+from axiom.AxiomLexer import *
+from axiom.AxiomParser import *
+    # from axiom.AxiomASTNodes import *
+from axiom.AxiomInterpreter import *
+
+from axiom.repl import repl
+
+
+
+def run_file(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            code = f.read()
+
+    except FileExistsError:
+        print(f'Error: File "{filename}" not found')
+        sys.exit(1)
+
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        sys.exit(1)
+
+
+    lexer = AxiomLexer(code)
+    parser = AxiomParser(lexer)
+    interpreter = AxiomInterpreter()
+
+    try:
+        while parser.current_token.type != AxiomTokenType.EOF:
+            stmt = parser.parse_statement()
+            interpreter.visit(stmt)
+
+    except Exception as e:
+
+        print(f'Runtime error: {e}')
+        sys.exit(1)
+
+
+
+
+def main():
+
+    if len(sys.argv) > 1:
+        run_file(sys.argv[1])
+
+    else:
+        repl()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

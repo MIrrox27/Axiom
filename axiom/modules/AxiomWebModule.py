9c27f667440d6fdc5c027f6912f8e3ee627337e0
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-04-02 14:48:54 +0500

    сделан класс для ошибок, логов и системных выводов

--- axiom/modules/AxiomWebModule.py
+++ axiom/modules/AxiomWebModule.py
@@ -2,16 +2,28 @@ import http, requests, urllib, webbrowser, socket, ssl
 import json, socketserver
 from http.server import HTTPServer, SimpleHTTPRequestHandler, BaseHTTPRequestHandler
 
+class Output:
+    def __init__(self, module):
+        self.module = module
 
-class WebModule(BaseHTTPRequestHandler):
+    def debug(self, msg, out=False):
+        if out: print(f"[DEBUG]: {msg}")
+
+    def error(self, msg, func=None):
+        raise Exception(f"[{self.module}]-[{func}]: {msg}")
+
+    def log(self, msg, out=False):
+        if out: print(f"[LOG]: {msg}")
+
+
+
+class WebHandler(BaseHTTPRequestHandler):
 
     def do_GET(self, headers=("Content-type", "text/html; charset=utf-8"), response_body=None):
         self.send_response(200)
         self.send_header(headers[0], headers[1])
         self.end_headers()
         self.wfile.write(response_body.encode("utf-8"))
-
-
 
 
     def do_POST(self):

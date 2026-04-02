585c2db7785665199df254d83f10aa667a822ffc
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-04-02 14:34:13 +0500

    сделан метод do_GET

--- axiom/modules/AxiomWebModule.py
+++ axiom/modules/AxiomWebModule.py
@@ -5,8 +5,14 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler, BaseHTTPRequestHan
 
 class WebModule(BaseHTTPRequestHandler):
 
-    def do_GET(self, headers=["Content-type", "text/html; charset=utf-8"]):
-        pass
+    def do_GET(self, headers=("Content-type", "text/html; charset=utf-8"), response_body=None):
+        self.send_response(200)
+        self.send_header(headers[0], headers[1])
+        self.end_headers()
+        self.wfile.write(response_body.encode("utf-8"))
+
+
+
 
     def do_POST(self):
         pass

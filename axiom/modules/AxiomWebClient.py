308b2177c3088a52aee6c54a4598831daf4f4eae
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-04-03 11:53:22 +0500

    изменена архитектура

--- axiom/modules/AxiomWebClient.py
+++
@@ -1,6 +0,0 @@
-from axiom.modules.AxiomWebModule import WebModule
-
-
-class ClientModule(WebModule):
-    def __init__(self):
-        pass
---
+++ axiom/modules/web/AxiomWebClient.py
@@ -0,0 +1,8 @@
+# author https://github.com/MIrrox27/Axiom
+
+from axiom.modules.web.AxiomWebModule import WebHandler
+
+
+class ClientModule(WebHandler):
+    def __init__(self):
+        pass

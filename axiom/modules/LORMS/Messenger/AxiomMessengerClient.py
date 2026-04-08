e67fe8f5e5dd2a7ebe28f915d364750fb86eb525
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-04-08 20:01:38 +0500

    добавлен класс клиента

--- axiom/modules/LORMS/Messenger/AxiomMessengerClient.py
+++ axiom/modules/LORMS/Messenger/AxiomMessengerClient.py
@@ -1,1 +1,14 @@
+# author https://github.com/MIrrox27/Axiom
+# AxiomMessengerClient.py
+
 from axiom.modules.LORMS.Messenger.AxiomMessenger import Messenger
+from axiom.modules.LORMS.Messenger.AxiomMessengerHandler import *
+
+
+
+class MessengerWebClient(MessengerHTTP): # для этой вариации библиотеки клиентом служит браузер пользователя
+    pass
+
+
+
+

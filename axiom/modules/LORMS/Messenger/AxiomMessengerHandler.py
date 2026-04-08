40cf2b2096910f68a43db448e62115b1fad9bf6b
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-04-08 20:03:19 +0500

    исправлены ошибки, изменена разметка, сделан тест

--- axiom/modules/LORMS/Messenger/AxiomMessengerHandler.py
+++ axiom/modules/LORMS/Messenger/AxiomMessengerHandler.py
@@ -1,3 +1,6 @@
+# author https://github.com/MIrrox27/Axiom
+# AxiomMessengerHandler.py
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from axiom.modules.LORMS.Messenger.AxiomMessenger import MessengerHTTP
 
@@ -5,21 +8,44 @@ from axiom.modules.LORMS.Messenger.AxiomMessenger import MessengerHTTP
 
 
 class MessengerHandlerHTTPChat(BaseHTTPRequestHandler):
-    temp = Messenger.templates.get('chat.html')
+    default_temp = MessengerHTTP.templates.get('chat')
 
     def do_GET(self):
-        try:
+        if self.path == '/stream':
+            print("/stream")
 
-            with open(self.temp, 'rb') as t:
+
+
+        try:
+            with open(self.default_temp, 'rb') as t:
                 content = t.read()
 
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
+                self.send_response(200)
 
-            self.wfile.write(content)
+                self.send_header("Content-type", "text/html")
+                #self.send_header("Cache-Control", "no-cache")
+                self.end_headers()
+
+                self.wfile.write(content)
 
         except FileNotFoundError:
             self.send_error(404, "File Not Found")
+
+
+    def do_POST(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    server = HTTPServer(('localhost', 1234), MessengerHandlerHTTPChat)
+    print("Server started...")
+    server.serve_forever()
+
+
+
+
+
+
 
 

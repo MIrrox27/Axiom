319e306f21be3fee79581ddb750ed1692b80ca77
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-04-08 19:28:34 +0500

    добавлены пояснения

--- axiom/modules/LORMS/AxiomLORMSModule.py
+++ axiom/modules/LORMS/AxiomLORMSModule.py
@@ -1,12 +1,15 @@
 import re, http, requests, html, urllib
 
-class LORMSModule:
-    def __init__(self):
-        pass
+class LORMSModule: # общий класс для всех модулей
+    def __init__(self): pass
 
 
 
-class Messenger(LORMSModule):
+
+
+
+
+class Messenger(LORMSModule): # общий класс для всех библиотек-мессенджеров
     def __init__(self): pass
 
 

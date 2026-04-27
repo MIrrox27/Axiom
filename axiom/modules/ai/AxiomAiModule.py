eb556336345e54ae80658f87bdbe2d5abd2310d9
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-04-27 18:43:53 +0500

    назначены функции, сделан файл для классов Ai модуля

---
+++ axiom/modules/ai/AxiomAiModule.py
@@ -0,0 +1,59 @@
+# author https://github.com/MIrrox27/Axiom
+# Ai Module
+# AxiomAiModule.py
+
+from openai import OpenAI
+
+
+"""
+    Список функций для реализации:
+        
+        - загрузка модели в формате ONNX (`var model = `)
+        - функция с определенным количеством запросов
+        
+            # текстовые модели
+        - отправка запросов модели и получение ответа
+        - создание контекста 
+        - очистка контекста 
+        
+            
+            # изображения 
+        - 
+        -
+"""
+
+
+
+
+
+class AiModule:
+    pass
+
+
+
+class Client(AiModule):
+    def __init__(self, api):
+        self.api = api
+
+
+
+class Ai(AiModule):
+    def __init__(self, model):
+        self.model = model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
